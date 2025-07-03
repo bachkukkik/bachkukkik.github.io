bb51e2e0a87d26d7924cf49f66f1e85d5931572e
bachkukkik <krichkorn@gmail.com>
2025-07-04 06:50:55 +0700

    add styles

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import arrr
-from pyscript import document
-
-
-def translate_english(event):
-    input_text = document.querySelector("#english")
-    english = input_text.value
-    output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
+from pyscript import display
+import datetime
+now = datetime.datetime.now()
+display(f"Hello from PyScript! The current time is: {now.strftime('%Y-%m-%d %H:%M:%S')}")
